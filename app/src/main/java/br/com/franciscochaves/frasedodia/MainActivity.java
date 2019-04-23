5246582bfbd0f2fd6ede0e315042288c23138a7c
Francisco Chaves <francisco.jschaves@gmail.com>
2019-04-23 14:41:28 -0300

    Gerando frases aleatórias

--- app/src/main/java/br/com/franciscochaves/frasedodia/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/frasedodia/MainActivity.java
@@ -2,12 +2,40 @@ package br.com.franciscochaves.frasedodia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textoNovaFrase;
+    private Button botaoNovaFrase;
+
+    private String[] frases = {
+            "Se você raçar metas absurdamente altas e falhar, seu fracasso será muito melhor que o sucesso de todos",
+            "O sucesso normalmente vem para que setá ocupado demais para procurar por ele",
+            "Se você não está disposto a arriscar, esteja disposto a uma vida comum"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textoNovaFrase = findViewById(R.id.text_nova_frase);
+        botaoNovaFrase = findViewById(R.id.button_nova_frase);
+
+        //textoNovaFrase.setText(frases[0]);
+        botaoNovaFrase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                int numeroAleatorio = random.nextInt(frases.length);
+
+                textoNovaFrase.setText(frases[numeroAleatorio]);
+            }
+        });
     }
 }
